9ddf48eb803c433aaaaa449399863d6ecac0f80b
Florian Wagner <fl.wagner@uni-tuebingen.de>
2021-04-08 15:49:22 +0000

    Correct make variable quoting.

--- poudomatic/worker/jinja2/filters.py
+++ poudomatic/worker/jinja2/filters.py
@@ -5,14 +5,24 @@ from shlex import quote as shquote
 class FiltersExtension(Extension):
     def __init__(self, environment):
         super().__init__(environment)
-        environment.filters["shquote"] = self._shquote
-        environment.filters["dirname"] = self._dirname
+        environment.filters.update(
+            shquote = self._shquote,
+            dirname = self._dirname,
+            makevar = self._makevar,
+        )
 
     def _shquote(self, arg):
         return shquote(str(arg))
 
     def _dirname(self, arg):
         return str(Path(arg).parent)
+
+    def _makevar(self, arg):
+        if (arg := str(arg)):
+            arg = arg.replace('\n', '${.newline}').replace('"', '\\"')
+            if arg[0] in ' \t' or arg[-1] in ' \t':
+                return f'"{arg}"'
+        return arg
 
 __all__ = (
     "FiltersExtension",
